faeb96dc10d263c55afdd5ca46af6c18bf8db4bd
Pawan9675 <pawankumarsingh3333@gmail.com>
2024-03-28 19:14:18 +0530

    25th commit

--- src/Week_8_Basic_Sorting/Assignment_Basic_Sorting/Minimum_Operation.java
+++ src/Week_8_Basic_Sorting/Assignment_Basic_Sorting/Minimum_Operation.java
@@ -27,7 +27,7 @@ public class Minimum_Operation {
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.print("Enter the elements of Array: ");
-        for (int i = 0; i <= n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
