6b40491c8a7283246539a7210cc2455c79013a72
Pawan9675 <pawankumarsingh3333@gmail.com>
2024-04-27 12:52:13 +0530

    34th commit

--- src/Week_10_Recursion/Generate_Parenthesis.java
+++ src/Week_10_Recursion/Generate_Parenthesis.java
@@ -3,13 +3,13 @@ package Week_10_Recursion;
 import java.util.Scanner;
 
 public class Generate_Parenthesis {
-    public static void printParenthesis(int open, int close, int n, String ans){
-        if (ans.length() == 2*n){
+    public static void printParenthesis(int open, int close, int n, String ans) {
+        if (ans.length() == 2 * n) {
             System.out.println("Valid Parentheses: " + ans);
             return;
         }
-        if (open < n) printParenthesis(open+1,close,n,ans+"(");
-        if (close < open) printParenthesis(open, close+1, n, ans+")");
+        if (open < n) printParenthesis(open + 1, close, n, ans + "(");
+        if (close < open) printParenthesis(open, close + 1, n, ans + ")");
     }
 
     public static void main(String[] args) {
