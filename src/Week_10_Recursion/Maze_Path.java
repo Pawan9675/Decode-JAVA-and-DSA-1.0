d08d9562330dceb46b1d1dfeaf891ae8b93094ef
Pawan9675 <pawankumarsingh3333@gmail.com>
2024-04-27 23:24:25 +0530

    34th commit

--- src/Week_10_Recursion/Maze_Path.java
+++ src/Week_10_Recursion/Maze_Path.java
@@ -3,24 +3,36 @@ package Week_10_Recursion;
 import java.util.Scanner;
 
 public class Maze_Path {
-    public static int mazePath1(int row, int col, int r, int c){
+    public static int mazePath1(int row, int col, int r, int c) {
         if (row == r || col == c) return 1;
         /*
         Another Base Case:
         if (row == r && col == c) return 1;
         if (row > r || col > c) return 0;
         */
-        int rightWays = mazePath1(row, col+1, r, c);
-        int downWays = mazePath1(row+1, col, r, c);
+        int rightWays = mazePath1(row, col + 1, r, c);
+        int downWays = mazePath1(row + 1, col, r, c);
         return rightWays + downWays;
     }
+
+    public static void printPath(int row, int col, int r, int c, String s) {
+        if (row > r || col > c) return;
+        if (row == r && col == c) {
+            System.out.println(s);      // Destination Reached
+            return;
+        }
+        printPath(row, col + 1, r, c, s + "R");     // Right
+        printPath(row + 1, col, r, c, s + "D");     // Down
+    }
+
     public static int mazePath2(int r, int c) {
         if (r == 1 || c == 1) return 1;
 
-        int rightWays = mazePath2(r, c-1);
-        int downWays = mazePath2(r-1, c);
+        int rightWays = mazePath2(r, c - 1);
+        int downWays = mazePath2(r - 1, c);
         return rightWays + downWays;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows of the maze: ");
@@ -30,5 +42,7 @@ public class Maze_Path {
 
         System.out.println("Number of paths through the maze: " + mazePath1(1, 1, r, c));
         System.out.println("Number of paths through the maze: " + mazePath2(r, c));
+        System.out.println("Paths through the maze: ");
+        printPath(1, 1, r, c, "");
     }
 }
--- src/Week_10_Recursion/Pre_In_Post.java
+++ src/Week_10_Recursion/Pre_In_Post.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Pre_In_Post {
     public static void pip(int num) {
         if (num == 0) return;
-        System.out.print("Pre " + num + " ");       // Pre
+        System.out.print("Pre: " + num + " ");       // Pre
         pip(num - 1);
-        System.out.print("In " + num + " ");        // In
+        System.out.print("In: " + num + " ");        // In
         pip(num - 1);
-        System.out.print("Post " + num + " ");      // Post
+        System.out.print("Post: " + num + " ");      // Post
     }
 
     public static void main(String[] args) {
