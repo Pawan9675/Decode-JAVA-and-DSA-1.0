39ec7c7ca630eee599e5da96b56040e28551dfa7
Pawan9675 <pawankumarsingh3333@gmail.com>
2024-08-24 00:25:17 +0530

    43th commit

--- src/Week_10_Recursion/Increasing_Sequence.java
+++ src/Week_10_Recursion/Increasing_Sequence.java
@@ -27,3 +27,40 @@ public class Increasing_Sequence {
         printSequences(n, k, 1, "");
     }
 }
+/*
+import java.util.Scanner;
+
+class Increasing_Sequence {
+    // Method to print all increasing sequences of length k
+    public static void printSequences(int n, int k, int start, String seq) {
+        // Base case: if the sequence has reached the desired length
+        if (seq.length() == k) {
+            System.out.println(seq);
+            return;
+        }
+
+        // Stop recursion if start exceeds n
+        if (start > n) {
+            return;
+        }
+
+        // Recur without including the current number
+        printSequences(n, k, start + 1, seq);
+
+        // Recur including the current number
+        printSequences(n, k, start + 1, seq + start);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter the length of the sequences (k): ");
+        int k = sc.nextInt();
+
+        // Start generating sequences from number 1
+        printSequences(n, k, 1, "");
+    }
+}
+*/
