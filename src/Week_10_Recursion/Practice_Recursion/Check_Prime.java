0bb33aeb2df009b54b0d51b4279a3acf48f77bb3
Pawan9675 <pawankumarsingh3333@gmail.com>
2024-09-10 00:39:25 +0530

    45th commit

--- src/Week_10_Recursion/Practice_Recursion/Check_Prime.java
+++ src/Week_10_Recursion/Practice_Recursion/Check_Prime.java
@@ -4,10 +4,15 @@ package Week_10_Recursion.Practice_Recursion;
 import java.util.Scanner;
 
 public class Check_Prime {
+
     public static boolean checkPrime(int num, int index) {
-        if (num <= 2) return num == 2;
+        // Base case: if num is less than 2, it's not a prime number.
+        if (num < 2) return false;
+        // Base case: if num is divisible by index, it's not a prime number.
         if (num % index == 0) return false;
+        // If index squared is greater than num, num is a prime number.
         if (index * index > num) return true;
+        // Recursive case: check the next possible divisor.
         return checkPrime(num, index + 1);
     }
 
